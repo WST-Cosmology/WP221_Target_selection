dd9b4ea1d054ac60c543a33513caaa2fb9fc031e
Constantin Payerne <cpayerne@login15.chn.perlmutter.nersc.gov>
2025-11-17 01:41:14 -0800

    rename

--- data/catalog_infos.py
+++ data/catalog_infos.py
@@ -8,3 +8,14 @@ get_COSMOS_wise_data 'curl -O "https://irsa.ipac.caltech.edu/workspace/TMP_AVox9
 
 extract_WISE_cosmos ='ra>148 and ra<153 and dec>0.5 and dec<4.5'
 
+
+#Les étoiles sont bien incluses dans les catalogues, et il y a plusieurs flag pour les identifier, tout depend du type de catalogue (SExtractor ou HSCpipe):
+#HSCpipe
+#- isCompact —> flag tous les objets PSF-likes (Existe pour toutes les bandes de HSC)
+#- isStarTemp —> flag les objets mieux fittés avec une SED d’étoiles que les SED de galaxies
+#- isStar —>  la combinaison “&” des deux flags
+
+#SExtractor
+#- OBJ_TYPE==2 —> flag les étoiles
+#- COMPACT==1 —> flag les objets compacts
+
