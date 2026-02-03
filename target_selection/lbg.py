3d60d5ef5e3fe384ce7a5cd7560acea6aa05a242
Constantin Payerne <cpayerne@login16.chn.perlmutter.nersc.gov>
2026-02-03 06:18:46 -0800

    new files

--- target_selection/lbg.py
+++ target_selection/lbg.py
@@ -1,76 +1,21 @@
 import numpy as np
 
-def refined_selection_fct(tab, conv):
-    
-    mask  = tab[conv['u_err']] > 0
-    mask *= tab[conv['g_err']] > 0
-    mask *= tab[conv['r_err']] > 0
+def LBG_SELECTION(tab, conv, name='COSMOS_TMG_U_normagcut'):
 
-    mask *= tab[conv['u_err']] < 1
-    mask *= tab[conv['g_err']] < 1
-    mask *= tab[conv['r_err']] < 1
-
-    mask *= abs(tab[conv['u']] - tab[conv['g']])  < 10
-    mask *= abs(tab[conv['g']] - tab[conv['r']])  < 10
-    mask *= abs(tab[conv['u']] - tab[conv['r']])  < 10
-    return mask
-
-def LBG_SELECTION(tab, conv, name='COSMOS_TMG_U'):
-
-    if name=='COSMOS_TMG_U': return COSMOS_TMG_U(tab, conv)
-    if name=='XMMLSS_uS_dropout': return XMMLSS_uS_dropout(tab, conv)
-    if name=='COSMOS_BXU_U': return COSMOS_BXU_U(tab, conv)
-    if name=='COSMOS_BXU_U_no_magr_cut': return  COSMOS_BXU_U_no_magr_cut(tab, conv)
-    if name=='COSMOS_BXU_U_ext': return COSMOS_BXU_U_ext(tab, conv)
+    if name=='COSMOS_TMG_U_normagcut': return COSMOS_TMG_U_normagcut(tab, conv)
+    if name=='COSMOS_BXU_U_normagcut': return  COSMOS_BXU_U_normagcut(tab, conv)
     if name=='COSMOS_BXU_U_ext_normagcut': return COSMOS_BXU_U_ext_normagcut(tab, conv)
-    if name=='COSMOS_G': return COSMOS_G(tab, conv)
+    if name=='XMMLSS_uS_dropout_normagcut': return XMMLSS_uS_dropout_normagcut(tab, conv)
+    if name=='COSMOS_G_noimagcut': return COSMOS_G_noimagcut(tab, conv)
+    if name=='COSMOS_R_nozmagcut': return COSMOS_R_nozmagcut(tab, conv)
 
-def XMMLSS_uS_dropout(tab, conv):
-
-    mask = tab[conv['u']] - tab[conv['g']] > 0.3
-    mask *= (0 < tab[conv['g']] - tab[conv['r']]) * (tab[conv['g']] - tab[conv['r']] < 0.8)
-    
-    mask_1 = (tab[conv['u']] - tab[conv['g']] > 2.0*(tab[conv['g']] - tab[conv['r']]) + 0.42)
-    mask_2 = ((tab[conv['u']] - tab[conv['g']] > 1.6*(tab[conv['g']] - tab[conv['r']]) + 0.55))
-    mask *= (mask_1 + mask_2)
-    
-    mask *= (22.7 < tab[conv['r']]) * (tab[conv['r']] < 24.2) 
-    return mask
-
-def COSMOS_TMG_U_ext(tab, conv):
-
+def COSMOS_TMG_U_normagcut(tab, conv):
     mask = tab[conv['u']] - tab[conv['g']] > 0.3
     mask *= (-0.5 < tab[conv['g']] - tab[conv['r']]) * (tab[conv['g']] - tab[conv['r']] < 1.)
     mask_1 = (tab[conv['u']] - tab[conv['g']] > 2.2*(tab[conv['g']] - tab[conv['r']]) + 0.32)
     mask_2 = ((tab[conv['u']] - tab[conv['g']] > 0.9) * (tab[conv['u']] - tab[conv['g']] > 1.6*(tab[conv['g']] - tab[conv['r']]) + 0.75))
     mask *= (mask_1 + mask_2)
-    mask *= (23.2 < tab[conv['r']]) * (tab[conv['r']] < 24.2) 
-    return mask
-
-def COSMOS_TMG_U(tab, conv):
-    mask = tab[conv['u']] - tab[conv['g']] > 0.3
-    mask *= (-0.5 < tab[conv['g']] - tab[conv['r']]) * (tab[conv['g']] - tab[conv['r']] < 1.)
-    mask_1 = (tab[conv['u']] - tab[conv['g']] > 2.2*(tab[conv['g']] - tab[conv['r']]) + 0.32)
-    mask_2 = ((tab[conv['u']] - tab[conv['g']] > 0.9) * (tab[conv['u']] - tab[conv['g']] > 1.6*(tab[conv['g']] - tab[conv['r']]) + 0.75))
-    mask *= (mask_1 + mask_2)
-    mask *= (22.5 < tab[conv['r']]) * (tab[conv['r']] < 23.75)
-    return mask
-
-def COSMOS_BXU_U_custom(tab, conv):
-    mask = tab[conv['u']] - tab[conv['g']] > 0
-    mask *= (-0.5 < tab[conv['g']] - tab[conv['r']]) * (tab[conv['g']] - tab[conv['r']] < 1.2)
-    mask_1 = (tab[conv['u']] - tab[conv['g']]  > 2.2*(tab[conv['g']] - tab[conv['r']]) + 0.32)
-    mask_2 = ((tab[conv['u']] - tab[conv['g']] > 0.9) * (tab[conv['u']] - tab[conv['g']] > 1.6*(tab[conv['g']] - tab[conv['r']]) + 0.75))
-    mask *= (mask_1 + mask_2)
-    return mask
-
-def COSMOS_BXU_U_ext(tab, conv):
-    mask = tab[conv['u']] - tab[conv['g']] > 0
-    mask *= (-0.5 < tab[conv['g']] - tab[conv['r']]) * (tab[conv['g']] - tab[conv['r']] < 1.2)
-    mask_1 = (tab[conv['u']] - tab[conv['g']]  > 2.2*(tab[conv['g']] - tab[conv['r']]) + 0.32)
-    mask_2 = ((tab[conv['u']] - tab[conv['g']] > 0.9) * (tab[conv['u']] - tab[conv['g']] > 1.6*(tab[conv['g']] - tab[conv['r']]) + 0.75))
-    mask *= (mask_1 + mask_2)
-    mask *= (23.75 < tab[conv['r']]) * (tab[conv['r']] < 24.2)
+    #mask *= (22.5 < tab[conv['r']]) * (tab[conv['r']] < 23.75)
     return mask
 
 def COSMOS_BXU_U_ext_normagcut(tab, conv):
@@ -81,16 +26,7 @@ def COSMOS_BXU_U_ext_normagcut(tab, conv):
     mask *= (mask_1 + mask_2)
     return mask
 
-def COSMOS_BXU_U(tab, conv):
-    mask = tab[conv['u']] - tab[conv['g']] > 0
-    mask *= (-0.5 < tab[conv['g']] - tab[conv['r']]) * (tab[conv['g']] - tab[conv['r']] < 1.2)
-    mask_1 = (tab[conv['u']] - tab[conv['g']]  > 2.2*(tab[conv['g']] - tab[conv['r']]) + 0.32)
-    mask_2 = ((tab[conv['u']] - tab[conv['g']] > 0.9) * (tab[conv['u']] - tab[conv['g']] > 1.6*(tab[conv['g']] - tab[conv['r']]) + 0.75))
-    mask *= (mask_1 + mask_2)
-    mask *= (23.75 < tab[conv['r']]) * (tab[conv['r']] < 24.5)
-    return mask
-
-def COSMOS_BXU_U_no_magr_cut(tab, conv):
+def COSMOS_BXU_U_normagcut(tab, conv):
     mask = tab[conv['u']] - tab[conv['g']] > 0
     mask *= (-0.5 < tab[conv['g']] - tab[conv['r']]) * (tab[conv['g']] - tab[conv['r']] < 1.2)
     mask_1 = (tab[conv['u']] - tab[conv['g']]  > 2.2*(tab[conv['g']] - tab[conv['r']]) + 0.32)
@@ -98,9 +34,31 @@ def COSMOS_BXU_U_no_magr_cut(tab, conv):
     mask *= (mask_1 + mask_2)
     return mask
 
-def COSMOS_G(tab, conv):
+def COSMOS_G_noimagcut(tab, conv):
     mask = tab[conv['g']] - tab[conv['r']] > 1.
     mask *= (-1.5 < tab[conv['r']] - tab[conv['i']]) * (tab[conv['r']] - tab[conv['i']] < 1.)
     mask *= (tab[conv['g']] - tab[conv['r']] > 1.5*(tab[conv['r']] - tab[conv['i']]) + 0.8) 
-    mask *= (22.5 < tab[conv['i']]) * (tab[conv['i']] < 25.5)
+    #mask *= (22.5 < tab[conv['i']]) * (tab[conv['i']] < 25.5)
+    return mask
+
+def COSMOS_R_nozmagcut(tab, conv):
+    mask = tab[conv['r']] - tab[conv['i']] > 1.7
+    mask *= tab[conv['r']] - tab[conv['i']] < 5
+    mask *= ((tab[conv['r']] - tab[conv['i']]) > 1.6 * (tab[conv['i']] - tab[conv['z']]) + 1.2)
+    mask *= tab[conv['i']] - tab[conv['z']] < 1.5
+    mask *= tab[conv['i']] - tab[conv['z']] > -0.5
+    #mask *= ((tab[conv['r']] - tab[conv['i']]) > 1.5*(tab[conv['i']] - tab[conv['z']]) + 1)
+    #mask *= (22.5 < tab[conv['i']]) * (tab[conv['i']] < 25.5)
+    return mask
+    
+def XMMLSS_uS_dropout_normagcut(tab, conv):
+
+    mask = tab[conv['u']] - tab[conv['g']] > 0.3
+    mask *= (0 < tab[conv['g']] - tab[conv['r']]) * (tab[conv['g']] - tab[conv['r']] < 0.8)
+    
+    mask_1 = (tab[conv['u']] - tab[conv['g']] > 2.0*(tab[conv['g']] - tab[conv['r']]) + 0.42)
+    mask_2 = ((tab[conv['u']] - tab[conv['g']] > 1.6*(tab[conv['g']] - tab[conv['r']]) + 0.55))
+    mask *= (mask_1 + mask_2)
+    
+    #mask *= (22.7 < tab[conv['r']]) * (tab[conv['r']] < 24.2) 
     return mask
