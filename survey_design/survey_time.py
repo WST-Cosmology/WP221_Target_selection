b7f90ec3dd1a28612460bb7f3fd84fa7a1e79845
Constantin Payerne <cpayerne@login26.chn.perlmutter.nersc.gov>
2026-05-11 02:35:49 -0700

    last changes for total survey time

--- survey_design/survey_time.py
+++ survey_design/survey_time.py
@@ -1,7 +1,18 @@
-texp_bright_time = 1000 #seconds
-texp_grey_time = 1000
-texp_dark_time = 
+import numpy as np
 
-def survey_time(Nmag_z, Spec_efficiency):
-
-    #return the survey time for a given tracer
+def passes_needed(Xtarget_init, Xfiber, X_I_want, max_passes=2000, coll=True, poiss=True):
+    nleft = []
+    Xremaining = Xtarget_init
+    cumulative = 0.0
+    nleft.append(Xtarget_init)
+    for n in range(1, max_passes + 1):
+        lambda_ = Xremaining/Xfiber
+        p_associated = 1-np.exp(-lambda_)
+        associated_this_pass = Xfiber
+        if poiss: associated_this_pass *= p_associated
+        observed_this_pass = associated_this_pass 
+        cumulative += observed_this_pass
+        Xremaining -= observed_this_pass
+        nleft.append(Xremaining)
+        if Xremaining < 1:
+            return n, cumulative, Xremaining, nleft
